c8d192f869cb7daed912e073394332326ecea9c5
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-06-21 13:20:30 -0300

    Add project files.

---
+++ LogViewer/Parsers/SuperFastParser.cs
@@ -0,0 +1,226 @@
+﻿
+using Microsoft.Win32.SafeHandles;
+using System.Buffers;
+using System.Collections.Immutable;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Unicode;
+
+namespace LogViewer.Parsers;
+
+public unsafe class SuperFastParser : IDisposable
+{
+	readonly FileStream stream;
+	readonly MemoryMappedFile mmf;
+	readonly MemoryMappedViewAccessor va;
+	readonly SafeMemoryMappedViewHandle vaHandle;
+	readonly byte* pointer;
+	readonly long fileLength;
+
+	readonly int initialChunkCount;
+	const int MaxChunkSize = int.MaxValue - 100_000;
+
+	public string FilePath { get; }
+
+	static double[] powersOf10 = new double[64];
+	static GCHandle powersHandle;
+	readonly double* powersPtr = Init10Powers();
+	ImmutableArray<LogLine2>? lines;
+
+	public SuperFastParser(string filePath, int? chunckCount = null)
+	{
+		ArgumentException.ThrowIfNullOrEmpty(filePath);
+		initialChunkCount = Math.Max(1, chunckCount ?? Environment.ProcessorCount);
+		FilePath = filePath;
+
+		stream = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1, FileOptions.SequentialScan);
+		fileLength = stream.Length;
+		mmf = MemoryMappedFile.CreateFromFile(filePath, FileMode.Open);
+
+		byte* ptr = (byte*)0;
+		va = mmf.CreateViewAccessor(0, fileLength, MemoryMappedFileAccess.Read);
+		vaHandle = va.SafeMemoryMappedViewHandle;
+		vaHandle.AcquirePointer(ref ptr);
+
+		pointer = ptr;
+	}
+
+	public List<(long start, int end)> SplitIntoMemoryChunks()
+	{
+		var chunkCount = initialChunkCount;
+		var chunkSize = fileLength / chunkCount;
+
+		while (chunkSize > MaxChunkSize)
+		{
+			chunkCount *= 2;
+			chunkSize = fileLength / chunkCount;
+		}
+
+		var chunks = new List<(long, int)>(chunkCount);
+		var pos = 0L;
+
+		for (var i = 0; i < chunkCount; i++)
+		{
+			if (pos + chunkSize >= fileLength)
+			{
+				chunks.Add((pos, (int)(fileLength - pos)));
+				break;
+			}
+
+			var newPos = pos + chunkSize;
+			var sp = new ReadOnlySpan<byte>(pointer + newPos, (int)chunkSize);
+			var idx = IndexOfNewLineChar(sp, out var stride);
+			newPos += idx + stride;
+			var len = newPos - pos;
+			chunks.Add((pos, (int)(len)));
+			pos = newPos;
+		}
+
+		return chunks;
+	}
+
+	public List<LogLine2> ProcessChunk(long start, int length)
+	{
+		var remaining = new Utf8Span(pointer + start, length);
+
+		var result = new List<LogLine2>(512);
+
+		while (remaining.Length > 0)
+		{
+			var idx = remaining.Span.IndexOf(newLine);
+			var value = remaining.SliceUnsafe(0, idx);
+			var log = new LogLine2(Encoding.UTF8.GetString(value.Span));
+			result.Add(log);
+			remaining = remaining.SliceUnsafe(idx + 1);
+		}
+
+		return result;
+	}
+
+	public List<LogLine2> ProcessChunkFilter(long start, int length, ReadOnlySpan<char> query)
+	{
+		var remaining = new Utf8Span(pointer + start, length);
+		var result = new List<LogLine2>(1024);
+
+		Span<char> lineCharBuffer = stackalloc char[1024];
+		char[]? lineArray = null;
+		while (remaining.Length > 0)
+		{
+			var idx = remaining.Span.IndexOf(newLine);
+			var lineLength = idx >= 0 ? idx : remaining.Length;
+			var value = remaining.SliceUnsafe(0, lineLength);
+
+			Span<char> decodedLine = lineCharBuffer;
+			int maxCharsNeeded = Encoding.UTF8.GetMaxCharCount(value.Length);
+
+			if (maxCharsNeeded > lineCharBuffer.Length)
+			{
+				if (lineArray is not null)
+				{
+					ArrayPool<char>.Shared.Return(lineArray);
+				}
+				lineArray = ArrayPool<char>.Shared.Rent(maxCharsNeeded);
+				decodedLine = lineArray;
+			}
+
+			int charsWritten = Encoding.UTF8.GetChars(value.Span, decodedLine);
+			ReadOnlySpan<char> lineAsChars = decodedLine[..charsWritten];
+
+			if (lineAsChars.Contains(query, StringComparison.OrdinalIgnoreCase))
+			{
+				var text = new string(lineAsChars);
+				var log = new LogLine2(text);
+				result.Add(log);
+			}
+
+			if (idx < 0) break;
+			remaining = remaining.SliceUnsafe(idx + 1);
+		}
+
+		if (lineArray is not null)
+		{
+			ArrayPool<char>.Shared.Return(lineArray);
+		}
+
+		return result;
+	}
+
+	public ImmutableArray<LogLine2> Filter(string query)
+	{
+		return SplitIntoMemoryChunks()
+		.AsParallel()
+		.AsOrdered()
+		.Select(tuple => ProcessChunkFilter(tuple.start, tuple.end, query))
+		.Aggregate((result, chunk) =>
+		{
+			result.AddRange(chunk);
+			return result;
+		})
+		.ToImmutableArray();
+	}
+
+	public ImmutableArray<LogLine2> Process()
+	{
+		lines ??= SplitIntoMemoryChunks()
+		.AsParallel()
+		.AsOrdered()
+		.Select(tuple => ProcessChunk(tuple.start, tuple.end))
+		.ToList()
+		.Aggregate((result, chunk) =>
+		{
+			result.AddRange(chunk);
+			return result;
+		}).ToImmutableArray();
+
+		return lines.Value;
+	}
+
+	const byte newLine = (byte)'\n';
+	const byte carriageReturn = (byte)'\r';
+	const byte semiCollon = (byte)';';
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal static int IndexOfNewLineChar(ReadOnlySpan<byte> span, out int stride)
+	{
+		stride = default;
+		var idx = span.IndexOfAny(newLine, carriageReturn);
+		if ((uint)idx < (uint)span.Length)
+		{
+			stride = 1;
+			if (span[idx] == carriageReturn)
+			{
+				var nextCharIdx = idx + 1;
+				if ((uint)nextCharIdx < (uint)span.Length && span[nextCharIdx] == newLine)
+				{
+					stride = 2;
+				}
+			}
+		}
+
+		return idx;
+	}
+
+
+	public void Dispose()
+	{
+		vaHandle.Dispose();
+		va.Dispose();
+		mmf.Dispose();
+		stream.Dispose();
+	}
+
+	static double* Init10Powers()
+	{
+		for (var i = 0; i < 64; i++)
+		{
+			powersOf10[i] = 1 / Math.Pow(10, i);
+		}
+
+		powersHandle = GCHandle.Alloc(powersOf10, GCHandleType.Pinned);
+		return (double*)powersHandle.AddrOfPinnedObject();
+	}
+}
+
+
---
+++ LogViewer/Parsers/Utf8Span.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace LogViewer.Parsers;
+
+[SkipLocalsInit]
+public unsafe struct Utf8Span : IEquatable<Utf8Span>
+{
+	internal byte* Pointer;
+	internal int Length;
+
+	public Utf8Span(byte* pointer, int length)
+	{
+		Debug.Assert(length >= 0);
+		Pointer = pointer;
+		Length = length;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal byte GetAtUnsafe(int idx) => Pointer[idx];
+
+	public ReadOnlySpan<byte> Span
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => new(Pointer, Length);
+	}
+
+	/// <summary>
+	/// Slice without bound checks. Use only when the bounds are checked/ensured before the call.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal Utf8Span SliceUnsafe(int start, int length) => new(Pointer + start, length);
+
+	/// <summary>
+	/// Slice without bound checks. Use only when the bounds are checked/ensured before the call.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal Utf8Span SliceUnsafe(int start) => new(Pointer + start, Length - start);
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public bool Equals(Utf8Span other) => Span.SequenceEqual(other.Span);
+
+	public override bool Equals(object? obj)
+	{
+		return obj is Utf8Span other && Equals(other);
+	}
+
+	public override int GetHashCode()
+	{
+		// Here we use the first 4 chars (if ASCII) and the length for a hash.
+		// The worst case would be a prefix such as Port/Saint and the same length,
+		// which for human geo names is quite rare. 
+
+		// .NET dictionary will obviously slow down with collisions but will still work.
+		// If we keep only `*_pointer` the run time is still reasonable ~9 secs.
+		// Just using `if (_len > 0) return (_len * 820243) ^ (*_pointer);` gives 5.8 secs.
+		// By just returning 0 - the worst possible hash function and linear search - the run time is 12x slower at 56 seconds. 
+
+		// The magic number 820243 is the largest happy prime that contains 2024 from https://prime-numbers.info/list/happy-primes-page-9
+
+		if (Length > 3)
+			return (Length * 820243) ^ (*(int*)Pointer);
+
+		if (Length > 1)
+			return *(short*)Pointer;
+
+		if (Length > 0)
+			return *Pointer;
+
+		return 0;
+	}
+
+	public short GetShortHashCode()
+	{
+		if (Length > 3)
+			return (short)((Length * 820243) ^ (*(int*)Pointer));
+
+		if (Length > 1)
+			return *(short*)Pointer;
+
+		if (Length > 0)
+			return *Pointer;
+
+		return 0;
+	}
+}
