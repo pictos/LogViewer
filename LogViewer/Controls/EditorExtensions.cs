52ea79c0606de3501a947678a1954dbe3001acac
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-06-23 19:08:22 -0300

    editor adjustments

---
+++ LogViewer/Controls/EditorExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+#endif
+
+namespace LogViewer.Controls;
+
+sealed class EditorExtensions
+{
+	public static void InitHandler()
+	{
+		EditorHandler.PlatformViewFactory = (handler) =>
+		{
+#if WINDOWS
+			var platformView = new TextBox
+			{
+				AcceptsReturn = true,
+				TextWrapping = TextWrapping.Wrap,
+				BorderThickness = new Microsoft.UI.Xaml.Thickness(0),
+				BorderBrush = new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Transparent),
+				FocusVisualPrimaryThickness = new Microsoft.UI.Xaml.Thickness(0),
+				FocusVisualSecondaryThickness = new Microsoft.UI.Xaml.Thickness(0)
+			};
+			platformView.Resources["TextControlBorderThemeThickness"] = new Microsoft.UI.Xaml.Thickness(0);
+			platformView.Resources["TextControlBorderThemeThicknessFocused"] = new Microsoft.UI.Xaml.Thickness(0);
+			platformView.Resources["TextControlBorderThemeThicknessPointerOver"] = new Microsoft.UI.Xaml.Thickness(0);
+			return platformView;
+#else
+			var platformEditor = new MauiTextView();
+
+			platformEditor.AddMauiDoneAccessoryView(handler);
+			handler.PlatformView.BorderStyle = UIKit.UITextViewBorderStyle.None;
+			return platformEditor;
+#endif
+		};
+	}
+}
--- LogViewer/MauiProgram.cs
+++ LogViewer/MauiProgram.cs
@@ -7,11 +7,19 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder.UseMauiApp<App>().UseNaluVirtualScroll().ConfigureFonts(fonts =>
-        {
-            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-        }).UseMauiCommunityToolkit();
+        builder
+            .UseMauiApp<App>()
+            .UseNaluVirtualScroll()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .ConfigureMauiHandlers(h =>
+            {
+                EditorExtensions.InitHandler();
+            })
+            .UseMauiCommunityToolkit();
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
