c10b2eea23ea35908288b0e92111eb59fb21e4ae
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-06-25 00:01:46 -0300

    create vm tests

--- LogViewer.DeviceTests/LogViewModelTests.cs
+++ LogViewer.DeviceTests/LogViewModelTests.cs
@@ -1,8 +1,5 @@
 ﻿using LogViewer.Parsers;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LogViewer.DeviceTests;
 
@@ -16,11 +13,63 @@ public class LogViewModelTests
 		using var reader = GenerateLoggerReader();
 		var vm = new ViewModels.LogViewModel(reader)
 		{
-			FileName= "app_lorem_ipsum"
+			FileName = "app_lorem_ipsum"
 		};
 
 		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(3110));
 		Assert.That(reader.FilePath.Contains(vm.FileName));
+	}
+
+	[Test]
+	public async Task TestSimpleFilter()
+	{
+		using var reader = GenerateLoggerReader();
+		var vm = new ViewModels.LogViewModel(reader)
+		{
+			FileName = "app_lorem_ipsum"
+		};
+
+		await vm.FilterCommand.ExecuteAsync("info");
+		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(668));
+	}
+
+	[Test]
+	public async Task TestFilterWithOr()
+	{
+		using var reader = GenerateLoggerReader();
+		var vm = new ViewModels.LogViewModel(reader)
+		{
+			FileName = "app_lorem_ipsum"
+		};
+		await vm.FilterCommand.ExecuteAsync("info | debug");
+		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(3011));
+	}
+
+	[Test]
+	public async Task TestFilterNot()
+	{
+		using var reader = GenerateLoggerReader();
+		var vm = new ViewModels.LogViewModel(reader)
+		{
+			FileName = "app_lorem_ipsum",
+			Query = "!info"
+		};
+
+		await vm.FilterCommand.ExecuteAsync(null);
+		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(2442));
+	}
+
+	[Test]
+	public async Task TestComplexFilter()
+	{
+		using var reader = GenerateLoggerReader();
+		var vm = new ViewModels.LogViewModel(reader)
+		{
+			FileName = "app_lorem_ipsum"
+		};
+
+		await vm.FilterCommand.ExecuteAsync("!info & error");
+		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(21));
 	}
 
 	static LoggerReader GenerateLoggerReader()
