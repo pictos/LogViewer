f7aa9a797c17f42a45d2a84f7f17f70774207f63
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-06-24 22:47:39 -0300

    add horizontalScroll

---
+++ LogViewer/UnsafeAccessorClass.cs
@@ -0,0 +1,17 @@
+﻿#if WINDOWS
+using Microsoft.UI.Xaml.Controls;
+#endif
+using Nalu;
+using System.Runtime.CompilerServices;
+
+namespace LogViewer;
+
+class UnsafeAccessorClass
+{
+#if WINDOWS
+	[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_scrollViewer")]
+	public static extern ref ScrollViewer GetScrollViewer(VirtualScrollHandler handler);
+#endif
+
+	UnsafeAccessorClass() { }
+}
