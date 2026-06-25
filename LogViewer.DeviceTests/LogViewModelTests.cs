20c017714a79c31420c0a605699711cb9bcc7f77
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-06-24 23:43:33 -0300

    start to create the LogViewModel tests

---
+++ LogViewer.DeviceTests/LogViewModelTests.cs
@@ -0,0 +1,33 @@
+﻿using LogViewer.Parsers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogViewer.DeviceTests;
+
+
+[TestFixture]
+public class LogViewModelTests
+{
+	[Test]
+	public void TestVmInit()
+	{
+		using var reader = GenerateLoggerReader();
+		var vm = new ViewModels.LogViewModel(reader)
+		{
+			FileName= "app_lorem_ipsum"
+		};
+
+		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(3110));
+		Assert.That(reader.FilePath.Contains(vm.FileName));
+	}
+
+	static LoggerReader GenerateLoggerReader()
+	{
+		var path = FullAppPackageFilePath;
+		var filePath = Path.Combine(path, "app_lorem_ipsum.txt");
+		using var reader = new LoggerReader(filePath);
+		return reader;
+	}
+}
