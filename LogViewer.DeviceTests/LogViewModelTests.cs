bc7d527cf52cfb7af56e5d5eff0f6dc288614e6f
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-06-25 00:16:14 -0300

    merge all filters into one place

--- LogViewer.DeviceTests/LogViewModelTests.cs
+++ LogViewer.DeviceTests/LogViewModelTests.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 
 namespace LogViewer.DeviceTests;
 
-
 [TestFixture]
 public class LogViewModelTests
 {
@@ -16,80 +15,30 @@ public class LogViewModelTests
 			FileName = "app_lorem_ipsum"
 		};
 
-
+		// Wait for the async initialization to complete
+		// It runs inside the ctor as a FireAndForget, 200ms is more than enough
+		// to everything be in place.
 		await Task.Delay(200);
 
 		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(3110));
 		Assert.That(reader.FilePath.Contains(vm.FileName));
 	}
 
-	[Test]
-	public async Task TestSimpleFilter()
+	[TestCase("info", 668)]
+	[TestCase("info | debug", 3011)]
+	[TestCase("!info", 2442)]
+	[TestCase("!info & error", 21)]
+	[TestCase("info & error", 0)]
+	public async Task TestFilter(string query, int expectedCount)
 	{
 		using var reader = GenerateLoggerReader();
 		var vm = new ViewModels.LogViewModel(reader)
 		{
 			FileName = "app_lorem_ipsum",
-			Query = "info"
-		};
-
-		await vm.FilterCommand.ExecuteAsync(null);
-		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(668));
-	}
-
-	[Test]
-	public async Task TestFilterWithOr()
-	{
-		using var reader = GenerateLoggerReader();
-		var vm = new ViewModels.LogViewModel(reader)
-		{
-			FileName = "app_lorem_ipsum",
-			Query = "info | debug"
+			Query = query
 		};
 		await vm.FilterCommand.ExecuteAsync(null);
-		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(3011));
-	}
-
-	[Test]
-	public async Task TestFilterNot()
-	{
-		using var reader = GenerateLoggerReader();
-		var vm = new ViewModels.LogViewModel(reader)
-		{
-			FileName = "app_lorem_ipsum",
-			Query = "!info"
-		};
-
-		await vm.FilterCommand.ExecuteAsync(null);
-		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(2442));
-	}
-
-	[Test]
-	public async Task TestComplexFilter()
-	{
-		using var reader = GenerateLoggerReader();
-		var vm = new ViewModels.LogViewModel(reader)
-		{
-			FileName = "app_lorem_ipsum",
-			Query = "!info & error"
-		};
-
-		await vm.FilterCommand.ExecuteAsync(null);
-		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(21));
-	}
-
-	[Test]
-	public async Task FilterWithAnd()
-	{
-		using var reader = GenerateLoggerReader();
-		var vm = new ViewModels.LogViewModel(reader)
-		{
-			FileName = "app_lorem_ipsum",
-			Query = "info & error"
-		};
-
-		await vm.FilterCommand.ExecuteAsync(null);
-		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(0));
+		Assert.That(vm.LogSource.GetItemCount(0), Is.EqualTo(expectedCount));
 	}
 
 	static LoggerReader GenerateLoggerReader()
