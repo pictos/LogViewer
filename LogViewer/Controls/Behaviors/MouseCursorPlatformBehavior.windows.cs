954d03809c9b17602894691dd604bdd9de129fa9
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-06-29 11:22:19 -0300

    create cursor handle

---
+++ LogViewer/Controls/Behaviors/MouseCursorPlatformBehavior.windows.cs
@@ -0,0 +1,42 @@
+﻿using LogViewer.Models;
+using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
+
+namespace LogViewer.Controls.Behaviors;
+
+partial class MouseCursorPlatformBehavior : PlatformBehavior<GridSplitter, UIElement>
+{
+	InputCursor wCursor = default!;
+	static readonly InputCursor arrowCursor = MouseCursor.Default.ToPlatform();
+
+	protected override void OnAttachedTo(GridSplitter bindable, UIElement platformView)
+	{
+		base.OnAttachedTo(bindable, platformView);
+
+		wCursor = HoverCursor.ToPlatform();
+
+		platformView.PointerEntered += OnPointerEntered;
+		platformView.PointerExited += OnPointerExited;
+	}
+
+	protected override void OnDetachedFrom(GridSplitter bindable, UIElement platformView)
+	{
+		base.OnDetachedFrom(bindable, platformView);
+		platformView.PointerExited -= OnPointerExited;
+		platformView.PointerEntered -= OnPointerEntered;
+	}
+
+	void OnPointerEntered(object sender, PointerRoutedEventArgs e)
+	{
+		var uiElement = (UIElement)sender;
+		
+		uiElement.ChangeCursor(wCursor);
+	}
+
+	static void OnPointerExited(object sender, PointerRoutedEventArgs e)
+	{
+		var uiElement = (UIElement)sender;
+		uiElement.ChangeCursor(arrowCursor);
+	}
+}
