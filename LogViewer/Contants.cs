de0197cfed634399a46ec8977625d695b75542e9
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-06-29 22:09:37 -0300

    create popup page and services

---
+++ LogViewer/Contants.cs
@@ -0,0 +1,7 @@
+﻿namespace LogViewer;
+
+public static class Contants
+{
+	public const string Versino = "X.X.X";
+	public const string Version = Versino;
+}
---
+++ LogViewer/Services/NavigationService.cs
@@ -0,0 +1,38 @@
+﻿using LogViewer.Pages;
+
+namespace LogViewer.Services;
+
+static class NavigationService
+{
+	static Shell Shell => Shell.Current;
+
+	static INavigation Navigation => Shell.Navigation;
+
+	public static async Task ShowPopupAsync(PopupPage popup)
+	{
+		ArgumentNullException.ThrowIfNull(popup, nameof(popup));
+
+		await RemovePopupAsync();
+		await MainThreadSwitcher.SwitchToMainThreadAsync();
+		await Navigation.PushModalAsync(popup, false);
+	}
+
+	public static async ValueTask RemovePopupAsync()
+	{
+		var modalStack = Shell.Navigation.ModalStack;
+		if (modalStack.Count is 0)
+		{
+			return;
+		}
+
+		var popup = modalStack[^1];
+
+		if (popup is not PopupPage)
+		{
+			return;
+		}
+
+		await MainThreadSwitcher.SwitchToMainThreadAsync();
+		await Navigation.PopModalAsync(false).ConfigureAwait(false);
+	}
+}
