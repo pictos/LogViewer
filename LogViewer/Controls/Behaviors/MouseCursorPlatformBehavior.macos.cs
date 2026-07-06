f685c1ab8978cb1e9f0cbaa0d0f2e02f4ec5991f
Pedro Jesus <pedrojesus.cefet@gmail.com>
2026-07-05 23:56:32 -0300

    implement MouseCursorPlatformBehavior

--- LogViewer/Controls/Behaviors/MouseCursorPlatformBehavior.macos.cs
+++ LogViewer/Controls/Behaviors/MouseCursorPlatformBehavior.macos.cs
@@ -6,7 +6,35 @@ namespace LogViewer.Controls.Behaviors;
 
 partial class MouseCursorPlatformBehavior : PlatformBehavior<VisualElement, UIView>
 {
+	 UIHoverGestureRecognizer? gestureRecognizer;
+	protected override void OnAttachedTo(VisualElement bindable, UIView platformView)
+	{
+		gestureRecognizer ??= new UIHoverGestureRecognizer((r) =>
+		{
+			switch (r.State)
+			{
+				case UIGestureRecognizerState.Began:
+					HoverCursor.ToPlatform().Set();
+					break;
+				case UIGestureRecognizerState.Changed:
+					break;
+				case UIGestureRecognizerState.Cancelled:
+				case UIGestureRecognizerState.Ended:
+					NSCursor.ArrowCursor.Set();
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		});
 
+		platformView.AddGestureRecognizer(gestureRecognizer);
+	}
+
+	protected override void OnDetachedFrom(VisualElement bindable, UIView platformView)
+	{
+		Assert(gestureRecognizer is not null);
+		platformView.RemoveGestureRecognizer(gestureRecognizer);
+	}
 }
 
 static class MouseCursorExtensions
